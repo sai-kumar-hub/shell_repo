9645f3c778030094b7c70cea4f15f0549736ae1b
root <root@ip-10-0-0-8.ap-south-1.compute.internal>
2024-11-01 12:10:29 +0000

    shell version 1.0

---
+++ shell.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Fun ASCII Art Hello World Script
+
+echo "HH   HH  EEEEE  LL      LL       OOO       WW      WW   OOO    RR    RR   LL      DDD   !!"
+echo "HH   HH  EE      LL      LL     OO   OO     WW      WW OO   OO  RR   RR    LL      DD DD !!"
+echo "HHHHHHH  EEEE    LL      LL    OO     OO     WW WW WW  OO     OO RRRRR      LL      DD  DD!!"
+echo "HH   HH  EE      LL      LL    OO     OO      WW  WW   OO     OO RR  RR     LL      DD  DD  "
+echo "HH   HH  EEEEE  LLLLLL  LLLLLL   OO OOO       WW WW      OO OOO  RR    RR   LLLLLL   DDD   !!"
+echo ""
+echo "Welcome to the world of Shell Scripting!"
